d4bbbff9d3a0e8e2aad9d0781658d2d64887b24a
kamalbuilds <geniusamansingh@gmail.com>
2024-11-11 17:16:20 +0700

    smart contract build success

--- smartcontract/hardhat.config.js
+++ smartcontract/hardhat.config.js
@@ -2,5 +2,13 @@ require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.24",
+  solidity: {
+    compilers: [
+      { version: "0.8.9" },
+      { version: "0.8.24" },
+      { version: "0.4.8" },
+      { version: "0.4.24" },
+      { version: "0.7.0" },
+    ]
+  }
 };
